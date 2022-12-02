42cdaf570f26a2d8f70e4d4545423910478bfe1b
fdubuisson <f.dubuisson@betclicgroup.com>
2022-12-02 14:51:35 +0100

    Naive implementation

--- src/main/kotlin/com/example/Routing.kt
+++ src/main/kotlin/com/example/Routing.kt
@@ -7,39 +7,45 @@ import org.slf4j.LoggerFactory
 
 fun Application.configureRouting() {
     val logger = LoggerFactory.getLogger(Application::class.java)
+    val elevator = Elevator()
 
     routing {
         get("/call") {
-            val atFloor = call.parameters["atFloor"]
-            val goTo = call.parameters["to"]
+            val atFloor = call.parameters["atFloor"]!!
+            val goTo = call.parameters["to"]!!
             logger.info("An user called the elevator at floor $atFloor to go $goTo")
 
+            elevator.onUserCall(atFloor.toInt(), Direction.valueOf(goTo))
             call.respondText("")
         }
         get("/go") {
-            val floorToGo = call.parameters["floorToGo"]
+            val floorToGo = call.parameters["floorToGo"]!!
             logger.info("An user in the elevator wants to goto floor $floorToGo")
 
+            elevator.onGoTo(floorToGo.toInt())
             call.respondText("")
         }
         get("/userHasEntered") {
             logger.info("An user has entered the elevator")
 
+            elevator.onUserEntered()
             call.respondText("")
         }
         get("/userHasExited") {
             logger.info("An user has exited the elevator")
 
+            elevator.onUserExited()
             call.respondText("")
         }
         get("/reset") {
-            val cause = call.parameters["cause"]
+            val cause = call.parameters["cause"]!!
             logger.info("Reset requested: $cause")
 
+            elevator.onReset(cause)
             call.respondText("")
         }
         get("/nextCommand") {
-            call.respondText("NOTHING")
+            call.respondText(elevator.requestNextCommand().name)
         }
     }
 }
